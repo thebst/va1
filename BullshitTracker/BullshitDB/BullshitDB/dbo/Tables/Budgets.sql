ccb15f4d707a960bd38b5d139f46942a11a8e830
Mark Degani <mark.degani@gmail.com>
2014-04-06 09:21:48 -0400

    Removed the db project.

--- BullshitTracker/BullshitDB/BullshitDB/dbo/Tables/Budgets.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[Budgets] (
-    [Id]         INT          IDENTITY (1, 1) NOT NULL,
-    [BudgetName] VARCHAR (50) NULL,
-    CONSTRAINT [PK_Budgets] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
--- BullshitTracker/BullshitDB/BullshitDB/dbo/Tables/Categories.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[Categories] (
-    [Id]   INT          IDENTITY (1, 1) NOT NULL,
-    [Name] VARCHAR (50) NULL,
-    CONSTRAINT [PK_Categories] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
