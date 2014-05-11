8ea989add2d828a95c39f0d67bd8ee5cd0ad16e7
Mark Degani <mark.degani@gmail.com>
2014-05-11 08:32:51 -0400

    added api and ms auth

--- BullshitTracker/BullshitTracker/App_Start/RouteConfig.cs
+++ BullshitTracker/BullshitTracker/App_Start/RouteConfig.cs
@@ -18,6 +18,8 @@ namespace BullshitTracker
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+
         }
     }
 }
--- BullshitTracker/BullshitTracker/App_Start/WebApiConfig.cs
+++ BullshitTracker/BullshitTracker/App_Start/WebApiConfig.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.OData.Builder;
+using BullshitTracker.Models;
 
 namespace BullshitTracker
 {
@@ -11,6 +13,8 @@ namespace BullshitTracker
         {
             // Web API configuration and services
 
+
+
             // Web API routes
             config.MapHttpAttributeRoutes();
 
@@ -18,7 +22,30 @@ namespace BullshitTracker
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
-            );
+            
+                
+                );
+
+            ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Transaction>("Trans");
+            builder.EntitySet<Account>("Accounts");
+           // builder.EntitySet<ExceptionTransaction>("ExceptionTransaction");
+            builder.EntitySet<Vendor>("Vendors");
+            builder.EntitySet<PurchasesByCategory>("PurchasesByCategory");
+            builder.EntitySet<ExceptionTransaction>("ExceptTrans");
+            builder.EntitySet<Category>("CategoryAPI");
+
+
+
+            //builder.EntitySet<Transaction>("Transactions");
+            builder.EntitySet<TaxRate>("TaxRates");
+
+            builder.EntitySet<Budget>("Budgets");
+
+
+
+            config.Routes.MapODataRoute("odata", "odata", builder.GetEdmModel());
+
         }
     }
 }
