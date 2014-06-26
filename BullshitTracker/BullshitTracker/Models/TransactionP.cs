9cbe3e4ba9fb71dc12d94ae1017f59aa12daef98
Mark Degani <mark.degani@gmail.com>
2014-06-26 13:29:00 -0400

    made transaction list pretty

---
+++ BullshitTracker/BullshitTracker/Models/TransactionP.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace BullshitTracker.Models
+{
+    [MetadataType(typeof(TransactionMetaData))]
+    public partial class Transaction
+    {
+    }
+
+    public partial class TransactionMetaData
+    {
+
+        [DisplayFormat(DataFormatString = "{0:#,##0.00}")]
+        public Nullable<decimal> Total { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:MMM dd, yyyy}",ApplyFormatInEditMode = true)]
+        public Nullable<System.DateTime> TransactionDate { get; set; }
+
+    }
+}
