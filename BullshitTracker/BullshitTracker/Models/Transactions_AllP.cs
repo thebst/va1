064f340e8de159c0834cd5e475ed3320f3fd8f65
Mark Degani <mark.degani@gmail.com>
2014-07-01 07:43:16 -0400

    Visual improvements on some pages

---
+++ BullshitTracker/BullshitTracker/Models/Transactions_AllP.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BullshitTracker.Models
+{
+    [MetadataType(typeof(Transactions_AllMetaData))]
+    public partial class Transactions_All
+    {
+    }
+
+    public partial class Transactions_AllMetaData
+    {
+        [DisplayFormat(DataFormatString = "{0:MMM dd, yyyy}", ApplyFormatInEditMode = true)]
+        public Nullable<System.DateTime> TransactionDate { get; set; }
+        
+        [DisplayFormat(DataFormatString = "{0:#,##0.00}")]
+        public Nullable<double> ActualAmount { get; set; }
+
+
+    }
+}
